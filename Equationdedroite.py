9bf0511203d8b55346b1201915c5cb6e4ead2d91
FlorianRopert <ropertflorian.etude@gmail.com>
2025-09-29 14:33:12 +0200

    CoMarhs

---
+++ Equationdedroite.py
@@ -0,0 +1,7 @@
+AbscissePa = int(input("Entrez xa"))
+OrdonnéePa = int(input("Entrez ya"))
+AbscissePb = int(input("Entrez xb"))
+OrdonnéePb = int(input("Entrez yb"))
+CoefDirect = (OrdonnéePb-OrdonnéePa)/(AbscissePb-AbscissePa)
+OrdoOrig= OrdonnéePa-CoefDirect* AbscissePa
+print(CoefDirect,"x+",OrdoOrig)
