d4575f1f0a4a1cc88eb26e61f62e4acf7b9ab89a
FlorianRopert <ropertflorian.etude@gmail.com>
2025-11-17 14:24:54 +0100

    Carre

--- MatPlot.py
+++ MatPlot.py
@@ -1,7 +1,15 @@
 from pylab import *
-xlim(0,10)
+#Triangle matplotlib
+"""xlim(0,10)
 ylim(0,10)
 x=[2,5,8,2]
 y=[2,8,2,2] 
+plot(x,y,linewidth=3,*"r")
+show()"""
+#Carre matplotlib
+xlim(0,10)
+ylim(0,10)
+x=[3,3,7,7,3]
+y=[3,7,7,3,3]
 plot(x,y,linewidth=3,*"r")
 show()
