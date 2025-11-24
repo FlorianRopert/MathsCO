f741a6a7f597f2fd29b52dda32f9300dd61e14a6
FlorianRopert <ropertflorian.etude@gmail.com>
2025-11-24 14:17:26 +0100

    Exo 4+5+6

--- MatPlot.py
+++ MatPlot.py
@@ -7,9 +7,40 @@ y=[2,8,2,2]
 plot(x,y,linewidth=3,*"r")
 show()"""
 #Carre matplotlib
-xlim(0,10)
+"""xlim(0,10)
 ylim(0,10)
 x=[3,3,7,7,3]
 y=[3,7,7,3,3]
 plot(x,y,linewidth=3,*"r")
-show()
+show()"""
+#Exercice 3
+"""xlim(0,20)
+ylim(2,4)
+y=3
+for i in range(10):
+    x=5+i
+    plot(x,y,"ob")
+show()"""
+#Exercice 4
+"""xlim(0,20)
+ylim(2,6)
+y=3
+for i in range(10):
+    x=5+i
+    plot(x,y,"ob")
+y=5
+for i in range(10):
+    x=5+i
+    plot(x,y,"ob")
+show()"""
+#Exercice 5
+"""xlim(0,20)
+ylim(0,30)
+y=10
+for i in range(9):
+    y+=1
+    for i in range(10):
+        x=5+i
+        plot(x,y,"ob")
+show()"""
+#Exercice 6
