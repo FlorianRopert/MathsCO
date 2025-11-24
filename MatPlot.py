1c92f6f1b79b9540365f87ccb1802f004391d606
FlorianRopert <ropertflorian.etude@gmail.com>
2025-11-24 14:27:42 +0100

    Ex 6+7+8

--- MatPlot.py
+++ MatPlot.py
@@ -44,3 +44,55 @@ for i in range(9):
         plot(x,y,"ob")
 show()"""
 #Exercice 6
+"""from pylab import *
+xlim(0,20)
+ylim(2,50,10)
+for i in range(31):
+    x=10+i
+    y=x
+    plot(x,y,'ob')
+show()"""
+#Exercice 7
+"""from pylab import *
+xlim(0,25,5)
+ylim(0,45,5)
+for i in range(21):
+    x=i
+    y_1=2*x
+    y_2=x
+    y_3=0.5*x
+    plot(x,y_1,'dr')
+    plot(x,y_2,'ob')
+    plot(x,y_3,'*r')
+show()"""
+#Exercice 8 - 1
+"""from pylab import *
+xlim(-3,5,1)
+ylim(-30,30,5)
+liste_x=[]
+liste_y=[]
+for i in range(100):
+    x=-3+0.06*i
+    y=x**3
+    liste_x.append(x)
+    liste_y.append(y)
+plot(liste_x,liste_y,'k')
+show()"""
+#Exercice 8 - 2
+"""from pylab import *
+xlim(-5,5,1)
+ylim(-50,50,1)
+liste_x=[]
+liste_y=[]
+for i in range(250):
+    x=-5+0.02*i
+    y=1/x
+    liste_x.append(x)
+    liste_y.append(y)
+for i in range(250):
+    x=0.02+0.02*i
+    y=1/x
+    liste_x.append(x)
+    liste_y.append(y)
+plot(liste_x,liste_y,'k')
+show()"""
